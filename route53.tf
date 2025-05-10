2ef5b07678814163bd219c3f1d48feb6620e2c87
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-05-10 08:19:44 -0300

    route53 loki

--- route53.tf
+++ route53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_zone" "private" {
     vpc_id = data.aws_ssm_parameter.vpc.value
   }
 }
+
+resource "aws_route53_record" "loki" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = format("loki.%s.local",  var.project_name)
+  type    = "CNAME"
+  ttl     = "30"
+  records = [aws_lb.loki.dns_name]
+}
