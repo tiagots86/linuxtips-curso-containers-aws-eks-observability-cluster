4c16ed3513df2c5f33546322e99c11db6ad86906
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-05-18 12:01:44 -0300

    route 53 tempo

--- lb_tempo.tf
+++ lb_tempo.tf
@@ -53,3 +53,11 @@ YAML
     helm_release.tempo
   ]
 }
+
+resource "aws_route53_record" "tempo" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = format("tempo.%s.local",  var.project_name)
+  type    = "CNAME"
+  ttl     = "30"
+  records = [aws_lb.loki.dns_name]
+}
